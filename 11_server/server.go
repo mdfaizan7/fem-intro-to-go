e9c6e7830e141e402a52d2218ad564bfb456742a
mdfaizan7 <faizaanazim@gmail.com>
2020-05-08 23:20:08 +0530

    mad e asmple todos app

--- 11_server/server.go
+++ 11_server/server.go
@@ -7,11 +7,30 @@ import (
 	"net/http"
 )
 
+// Todo is rthe struct containng content and title
+type Todo struct {
+	Title   string
+	Content string
+}
+
+// PageVariables are sent to hmtl template
+type PageVariables struct {
+	PageTitle string
+	PageTodos []Todo
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Home!")
 }
 
-func todos(w http.ResponseWriter, r *http.Request) {
+var todos []Todo
+
+func getTodos(w http.ResponseWriter, r *http.Request) {
+	pageVariables := PageVariables{
+		PageTitle: "Get TODOS",
+		PageTodos: todos,
+	}
+
 	t, err := template.ParseFiles("todos.html")
 
 	if (err) != nil {
@@ -19,12 +38,31 @@ func todos(w http.ResponseWriter, r *http.Request) {
 		log.Print("Template parsing error: ", err)
 	}
 
-	err = t.Execute(w, nil)
+	err = t.Execute(w, pageVariables)
+}
+
+func addTodo(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print("request parsingerror: ", err)
+	}
+
+	todo := Todo{
+		Title:   r.FormValue("title"),
+		Content: r.FormValue("content"),
+	}
+
+	todos = append(todos, todo)
+	log.Print(todos)
+	http.Redirect(w, r, "/todos", http.StatusSeeOther)
 }
 
 func main() {
 	http.HandleFunc("/", home)
-	http.HandleFunc("/todos/", todos)
+	http.HandleFunc("/todos/", getTodos)
+	http.HandleFunc("/add-todo/", addTodo)
 	fmt.Println("Server is running on port: 127.0.0.1:8080")
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
 }
